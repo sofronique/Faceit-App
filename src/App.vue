3f06df80be49b08f45c364e292a0400a7208b826
Panagiotis Sofroniadis <80383566+sofronique@users.noreply.github.com>
2026-01-30 15:50:56 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Search from './components/Search.vue';
+</script>
+
+<template>
+  <Search></Search>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,50 @@
+<script setup>
+import {ref} from 'vue'
+const playerNick = ref('')
+let playerObject = ref({})
+
+playerObject = {
+    name: '',
+    country: '',
+    created: ''
+}
+
+async function onSubmit(){
+const apiKey = 'b188a03d-3f2a-4af9-bee7-8f576041f40a';
+const url = 'https://open.faceit.com/data/v4/players?nickname=';
+
+fetch(url + playerNick.value, {
+  method: 'GET',
+  headers: {
+    'accept': 'application/json',
+    'Authorization': `Bearer ${apiKey}`
+  }
+}).then(response => response.json())
+  .then(data => {
+
+    if (playerObject) {playerObject = {
+    name: '',
+    country: '',
+    created: ''
+}}
+
+    playerObject.value = data
+    playerObject.country = data.country
+    playerObject.name = data.nickname
+    playerObject.created = data.activated_at?.slice(0, 10)
+
+    playerNick.value = ''
+  })}
+</script>
+
+<template>
+    <h1>Search for a user</h1>
+    <input v-model="playerNick"/>
+    <input type="button" @click="onSubmit()" value="Search">
+
+    <div v-if="playerObject">
+        <h2>{{playerObject.name}}</h2>
+        <h2>{{playerObject.country}}</h2>
+        <h2>{{playerObject.created}}</h2>
+    </div>
+</template>
